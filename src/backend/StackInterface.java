1b94baf2ced78e1e7ec7cbd08d48be4b6b5546ee
Johan Rickardo A. Roxas <2233375@slu.edu.ph>
2023-10-12 19:29:00 +0800

    Initial code for StackInterface.java

---
+++ src/backend/StackInterface.java
@@ -0,0 +1,41 @@
+package backend;
+
+/**
+ * @author ROXAS, Johan Rickardo
+ * @version 1.00 (12 October 2023)
+ * An interface representing a generic stack data structure.
+ * @param <T> The type of elements in the stack.
+ */
+public interface StackInterface<T> {
+    /**
+     * TODO: Documentation
+     * @param item
+     */
+    public void push(T item);
+
+    /**
+     * TODO: Documentation
+     * @return
+     * @throws StackUnderflowException
+     */
+    public T pop() throws StackUnderflowException;
+
+    /**
+     * TODO: Documentation
+     * @return
+     * @throws StackUnderflowException
+     */
+    public T peek() throws StackUnderflowException;
+
+    /**
+     * TODO: Documentation
+     * @return The size of the stack.
+     */
+    public int size();
+
+    /**
+     * TODO: Documentation
+     * @return
+     */
+    public boolean isEmpty();
+}
