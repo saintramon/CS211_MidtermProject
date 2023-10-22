593f461a4ec54bf401d44b78e188a0fe171ae737
RAMON EMMIEL JASMIN <2230748@slu.edu.ph>
2023-10-22 16:24:07 +0800

    Added attributes and constructors for Node class in Project 2

--- src/Project2/backend/Node.java
+++ src/Project2/backend/Node.java
@@ -1,4 +1,29 @@
 package Project2.backend;
 
 public class Node {
+    private char symbol;
+    private int count;
+    private Node left;
+    private Node right;
+
+    public Node(){
+        this.symbol = 'x';
+        this.count = 0;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Node(char symbol, int count){
+        this.symbol = symbol;
+        this.count = count;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Node(char symbol, int count, Node left, Node right){
+        this.symbol = symbol;
+        this.count = count;
+        this.left = left;
+        this.right = right;
+    }
 }
